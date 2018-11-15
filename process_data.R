64136bd0e50d6e83265d623362a7231db5803ecd
Esteban Arellano <Esteban@Estebans-MacBook-Pro.local>
2018-11-14 21:50:45 -0500

    education.rds

--- process_data.R
+++ process_data.R
@@ -29,6 +29,9 @@ education <- upshot %>%
   group_by(state_district) %>%
   count(educ) %>%
   spread(educ, n) %>%
-  mutate(higher_ed = "Graduate or Professional Degree" / ("Bachelors' degree" + "Grade school" + "Graduate or Professional Degree" + "High school" + "Some college or trade school") * 100) %>%
+  replace(is.na(.), 0) %>%
+  mutate(higher_ed = `Graduate or Professional Degree` / (`Bachelors' degree` + `Grade school` + `Graduate or Professional Degree` + `High school` + `Some college or trade school`) * 100) %>%
   left_join(JS, by = "state_district") %>%
-  select(state_district, dem_margin, non_white)
+  select(state_district, dem_margin, higher_ed)
+
+write_rds(education, "education.rds")
